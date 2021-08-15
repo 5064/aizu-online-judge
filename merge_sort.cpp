9ecf4e7ea89e3d6aba0fbcd0146c0b8fd34df1e9
5064 <5og4.kirin@gmail.com>
2021-08-15 11:10:13 +0900

    solve Merge Sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,36 +3,68 @@ using namespace std;
 #define SENTINEL 1024000000
 #define MAX 500000
 
-int S[MAX];
-int L[MAX / 2 + 2], R[MAX / 2 + 2];
+int L[MAX / 2 + 2], R[MAX / 2 + 2]; // 一時的なメモリ領域が必要
+int cnt;
 
 void merge(int A[], int n, int left, int mid, int right)
 {
+    int n1 = mid - left;
+    int n2 = right - mid;
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = A[left + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        R[i] = A[mid + i];
+    }
+    L[n1] = R[n2] = SENTINEL;
+    int i = 0, j = 0;
+    for (int k = left; k < right; k++)
+    {
+        cnt++;
+        if (L[i] <= R[j])
+        {
+            A[k] = L[i++];
+        }
+        else
+        {
+            A[k] = R[j++];
+        }
+    }
 }
 
-void mergeSort(int A[], int left[], int right[])
+void mergeSort(int A[], int n, int left, int right)
 {
+    if (left + 1 < right) // 部分配列の長さが3以上(1+2 or 2+1)のとき
+    {
+        int mid = (left + right) / 2;
+        mergeSort(A, n, left, mid);
+        mergeSort(A, n, mid, right);
+        merge(A, n, left, mid, right);
+    }
 }
 
 int main()
 {
     int n = 0;
+    int A[MAX];
     /* 標準入力受け取り */
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &S[i]);
+        scanf("%d", &A[i]);
     }
-    scanf("%d", &q);
-    int key, sum = 0;
-    for (int i = 0; i < q; i++)
+    mergeSort(A, n, 0, n);
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d", &key);
-        if (binarySearch(S, n, key))
+        if (i)
         {
-            sum++;
+            cout << " ";
         }
+        cout << A[i];
     }
-    printf("%d\n", sum);
+    cout << endl;
+    cout << cnt << endl;
 }
