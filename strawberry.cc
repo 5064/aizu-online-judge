0927cbbfd962f8b2a8178c69ee2726852f6429d6
5064 <5og4.kirin@gmail.com>
2021-08-17 16:41:34 +0900

    solve Strawberry

---
+++ strawberry.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for (ll i = 0; i < n; ++i)
+#define rep2(i, n) for (ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for (ll i = a; i < b; ++i)
+#define rep4(i, a, b, c) for (ll i = a; i < b; i += c)
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+//x:コンテナ
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b; // aをbで更新
+        return true;
+    }
+    return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b; // aをbで更新
+        return true;
+    }
+    return false;
+}
+
+//---------------------------------------
+
+int main(int argc, char const *argv[])
+{
+    int n;
+
+    cin >> n;
+    vector<pair<int, int>> S(n);
+
+    rep(i, n)
+    {
+        int a, t;
+        cin >> a >> t;
+        S[i].first = a;
+        S[i].second = max(a, t);
+    }
+    int ans = -1;
+    rep(i, n)
+    {
+        chmax(ans, (S[i].first + S[i].second));
+    }
+    cout << ans << endl;
+}
